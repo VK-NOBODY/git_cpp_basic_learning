e93486905ab445938e078cc909a91ea18109fab0
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 16:04:41 +0800

    add basic_use_of_variables_learn.cpp

---
+++ basic_use_of_variables_learn.cpp
@@ -0,0 +1,22 @@
+//
+// Created by VULCA on 30/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+    int age;
+    float height;
+    char gender;
+    string name;
+    age = 21;
+    height = 180.5;
+    gender = 'm';
+    name = "Ming";
+    cout << "age = " << age << endl;
+    cout << "height = " << height << endl;
+    cout << "gender = " << gender << endl;
+    cout << "name = " << name << endl;
+    return 0;
+}
