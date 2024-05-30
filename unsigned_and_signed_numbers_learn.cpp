2d2ca76c9312c01e39bcc8145fe034189e405fec
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 18:59:57 +0800

    update unsigned_and_signed_numbers_learn.cpp

--- unsigned_and_signed_numbers_learn.cpp
+++ unsigned_and_signed_numbers_learn.cpp
@@ -1,3 +1,14 @@
 //
 // Created by VULCA on 30/5/2024.
 //
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+    signed int num1 = 10;
+    int num2 = -10;
+    cout << num1 << "," << num2 << endl;
+    unsigned int num3 = -30;
+    cout << num3 << endl;
+}
