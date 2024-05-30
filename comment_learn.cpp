d9821f88d5623d27153baf3c9e72d36fd85c2c3c
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 15:18:40 +0800

    add comment_learn.cpp

---
+++ comment_learn.cpp
@@ -0,0 +1,15 @@
+//
+// Created by VULCA on 30/5/2024.
+//
+#include "useful_header_file.h" //Reference header file
+using namespace std; //Use namespace std
+
+int main() {
+    cout << "I am nobody." << endl;
+    cout << "I am losser." << endl;
+    cout << "I'm just a person living in self-deception." << endl;
+    /*
+     * This is not a lie
+     */
+    return 0;
+}
