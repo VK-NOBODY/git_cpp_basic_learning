3b36ca3b6887ce2b219b2bfa9ff4fc49de42607c
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 15:29:42 +0800

    add literal_constant_learn.cpp

---
+++ literal_constant_learn.cpp
@@ -0,0 +1,28 @@
+//
+// Created by VULCA on 30/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+    //int
+    21;
+    //float
+    180.5;
+    //char
+    'c';
+    //string
+    "";//empty string
+    "c";
+    "nobody";
+
+    cout << 21 << endl;
+    cout << 180.5 << endl;
+    cout << 'c' << endl;
+    cout << "" << endl;
+    cout << "c" << endl;
+    cout << "nobody" << endl;
+    return 0;
+
+}
