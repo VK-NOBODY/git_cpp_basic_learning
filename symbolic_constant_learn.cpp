3c5f15c24526cd6554385f85c9ec14b605304a8d
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-30 15:54:05 +0800

    add symbolic_constant_learn.cpp

--- symbolic_constant_learn.cpp
+++ symbolic_constant_learn.cpp
@@ -2,17 +2,23 @@
 // Created by VULCA on 30/5/2024.
 //
 #include "useful_header_file.h"
+//#include "windows.h"
 
 using namespace std;
 #define FAT_BMI 28
 #define J2C_RATE 4.19
+#define CHOU_AGE 21
+#define CHOU_HEIGHT 180.5
+#define CHOU_WEIGHT 56
 
 int main() {
 
-    system("chcp 65001");
+//    SetConsoleOutputCP(CP_UTF8);
     //Statements using symbolic constants are normal code and should be written in the main function.
     //How to use: To use symbolic constants, just use its name.
     cout << FAT_BMI << endl;
-    cout << "焦耳轉卡路里需要除以:" << J2C_RATE << endl;
+    cout << "To convert joules to calories you need to divide by:" << J2C_RATE << endl;
+    cout << "I am Chou." << "I am" << CHOU_AGE << " age." << endl;
+    cout << "my height is " << CHOU_HEIGHT << "cm," << "my weight is " << CHOU_WEIGHT << "kg." << endl;
     return 0;
 }
