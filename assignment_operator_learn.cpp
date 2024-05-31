875099f72227f4da09d4e260067bb25f1307ec8a
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-31 16:02:17 +0800

    update assignment_operator_learn.cpp

---
+++ assignment_operator_learn.cpp
@@ -0,0 +1,28 @@
+//
+// Created by VULCA on 31/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    int num_01 = 2;
+    num_01 = num_01 + 3;
+    cout << num_01 << endl;
+    num_01 += 3; //Equivalent to num_01 = num_01 + 3
+    cout << num_01 << endl;
+
+    num_01 -= 3;
+    cout << num_01 << endl;
+
+    num_01 *= 5;
+    cout << num_01 << endl;
+
+    num_01 /= 5;
+    cout << num_01 << endl;
+
+    num_01 %= 3;
+    cout << num_01 << endl;
+    return 0;
+}
