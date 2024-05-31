c14576d92e91daed6df64065495627f717b6c35b
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-31 11:17:58 +0800

    update data_type - character_learn.cpp

---
+++ data_type - character_learn.cpp
@@ -0,0 +1,17 @@
+//
+// Created by VULCA on 31/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    char ch = 97;
+    cout << ch << endl;
+    char ch_2 = 'a';
+    cout << ch_2 + 1 << endl;
+    char ch_3 = 'a' + 2;
+    cout << ch_3 << endl;
+    return 0;
+}
