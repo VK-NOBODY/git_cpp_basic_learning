2b7977e2251210d28635f7652666fa6f419cd69d
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-31 15:22:10 +0800

    update cin_data_input_learn.cpp

---
+++ cin_data_input_learn.cpp
@@ -0,0 +1,26 @@
+//
+// Created by VULCA on 31/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    //cin >> variable;
+    cout << "Please enter an integer: " << endl;
+    int num_01;
+    cin >> num_01;
+    cout << num_01 << endl;
+
+    cout << "Please enter a floating point number: " << endl;
+    double num_02;
+    cin >> num_02;
+    cout << num_02 << endl;
+
+    cout << "Please enter a character: " << endl;
+    char num_03;
+    cin >> num_03;
+    cout << num_03 << endl;
+    return 0;
+}
