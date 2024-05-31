2731f2887d29b27f9883165ea5341e9d6c68f38c
Vulcankta <Vulcankta@users.noreply.github.com>
2024-05-31 11:43:44 +0800

    update data_type_string_learn.cpp

---
+++ data_type_string_learn.cpp
@@ -0,0 +1,26 @@
+//
+// Created by VULCA on 31/5/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    char example_01[] = "nobody"; //Form of character array
+    char *example_02 = "noone";//String in pointer form
+
+    string example_03 = "C++ string";//C++ string type string
+
+//    example_01 = "666";
+    example_02 = "777";
+    example_03 = "888";
+
+    cout << example_01 << endl;
+    cout << example_02 << endl;
+    cout << example_03 << endl;
+
+
+    return 0;
+
+}
