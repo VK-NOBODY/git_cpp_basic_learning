e653af40f0e28f0fcbe1dd419c6de47e66a669f6
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-01 15:51:51 +0800

    update logical_operators_learn.cpp

---
+++ logical_operators_learn.cpp
@@ -0,0 +1,28 @@
+//
+// Created by VULCA on 1/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+    // ! No
+    bool example_01 = !(1 == 1);
+    bool example_02 = !(1 == 2);
+    cout << example_01 << endl;
+    cout << example_02 << endl;
+
+    // && and
+    bool example_03 = 1 == 1 && 2 == 2;
+    bool example_04 = 1 == 2 && 2 == 2;
+    cout << example_03 << endl;
+    cout << example_04 << endl;
+
+    // || or
+    bool example_05 = 1 == 1 || 1 == 2;
+    bool example_06 = 1 == 2 || 1 == 2;
+    cout << example_05 << endl;
+    cout << example_06 << endl;
+
+    return 0;
+}
