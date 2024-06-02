23bd47b2ea23ee5e7fcee77fcc157712f95c8218
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-02 16:02:03 +0800

    update while_loop_statement_learn.cpp and while_loop_statement_exercises_learn.cpp

---
+++ while_loop_statement_exercises_learn.cpp
@@ -0,0 +1,18 @@
+//
+// Created by VULCA on 2/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+//while loop, 100 times, each time there is a variable, the variable value is 1 for the first time, 2 for the second time, 3 for the third time, 4 for the fourth time, and so on, until 100 times
+    short number = 0;
+    short control_flag = 1;
+    while (control_flag <= 100) {
+        number += control_flag++;
+        cout << "number=" << number << endl;
+    }
+    return 0;
+}
---
+++ while_loop_statement_learn.cpp
@@ -0,0 +1,26 @@
+//
+// Created by VULCA on 2/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+//    //Loop control factor
+//    bool is_run = true;
+//    int number = 0;
+//    while (is_run) {
+//        cout << "hello" << endl;
+//        ++number;
+//        if (number > 5) {
+//            is_run = false;//Control factor update
+//        }
+//    }
+//I want to confess my love to Xiaomei, every day, for 10 consecutive days
+    int day = 1;
+    while (day <= 10) {
+        cout << "day: " << day << ", Xiaomei, I like you." << endl;
+        ++day;
+    }
+    return 0;
+}
