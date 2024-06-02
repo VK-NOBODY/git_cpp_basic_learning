d8af022effde4a89594d94573bbe0f2d999579e9
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-02 18:25:38 +0800

    update while_nested_loop_case_(ninety-nine multiplication tables)_learn.cpp

---
+++ while_nested_loop_case_(ninety-nine multiplication tables)_learn.cpp
@@ -0,0 +1,21 @@
+//
+// Created by VULCA on 2/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+// 1. Output nine lines through the outer loop
+    for (int i = 1; i <= 9; i++) {
+        // 2. Output nine columns through the inner loop
+        for (int j = 1; j <= i; j++) {
+            // 3. Output the multiplication table
+            cout << j << "x" << i << "=" << j * i << "\t";
+        }
+        // 4. Line break
+        cout << endl;
+    }
+
+    return 0;
+}
