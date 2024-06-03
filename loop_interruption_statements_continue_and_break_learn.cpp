780b58b837c6bbb479e7d056979b8a9a1cdf2805
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-03 17:34:06 +0800

    update loop_interruption_statements_continue_and_break_learn.cpp

---
+++ loop_interruption_statements_continue_and_break_learn.cpp
@@ -0,0 +1,33 @@
+//
+// Created by VULCA on 3/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+//    for (int i = 1; i <= 20; ++i) {
+//        if (i % 2 == 0) {
+//            continue;
+//        }
+//        cout << i << endl;
+//    }
+//    for (int i = 1; true; ++i) {
+//        cout << i << endl;
+//        if (i == 20) {
+//            break;
+//        }
+//    }
+    for (int i = 1;; ++i) {
+        if (!(i % 2 == 0)) {
+            continue;
+        }
+        cout << i << endl;
+        if (i == 300) {
+            break;
+        }
+    }
+
+    return 0;
+}
