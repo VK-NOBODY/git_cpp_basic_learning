3628ac3f6505d227014db9f9369c626bb9ca004e
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-04 17:08:33 +0800

    update  array_element_removal_exercises_learn.cpp and removal_of_array_elements_learn.cpp

---
+++ array_element_removal_exercises_learn.cpp
@@ -0,0 +1,29 @@
+//
+// Created by VULCA on 4/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    int *p_array = new
+            int[10]{3, 5, 1, 11, 99, 66, 22, 2, 8, 6};
+    int *p_new_array = new int[8];
+    int offset = 0;
+    for (int i = 0; i < 10; ++i) {
+        if (i == 0 || i == 5) {
+            ++offset;
+            continue;
+        } else {
+            p_new_array[i - offset] = p_array[i];
+        }
+    }
+    delete[] p_array;
+    cout << "The new array is: ";
+    for (int i = 0; i < 8; ++i) {
+        cout << p_new_array[i] << " ";
+    }
+    delete[] p_new_array;
+    return 0;
+}
---
+++ removal_of_array_elements_learn.cpp
@@ -0,0 +1,31 @@
+//
+// Created by VULCA on 4/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+    int *p_array = new int[5]{1, 3, 5, 7, 9};
+
+    int *p_new_array = new int[4];
+    for (int i = 0; i < 5; ++i) {
+        if (i == 2) {
+            continue;
+        }
+        if (i > 2) {
+            p_new_array[i - 1] = p_array[i];
+        } else {
+            p_new_array[i] = p_array[i];
+        }
+    }
+    delete[] p_array;
+    p_array = p_new_array;
+    for (int i = 0; i < 4; ++i) {
+        cout << "The elements in the new array are: " << p_new_array[i] << endl;
+        cout << "The elements in the original array are: " << p_array[i] << endl;
+    }
+    delete[] p_new_array;
+    return 0;
+}
