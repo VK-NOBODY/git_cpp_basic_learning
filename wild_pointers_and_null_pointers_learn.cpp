dd0881863339c901667be02fa4c3715f0498266c
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-04 11:24:02 +0800

    update wild_pointers_and_null_pointers_learn.cpp

---
+++ wild_pointers_and_null_pointers_learn.cpp
@@ -0,0 +1,21 @@
+//
+// Created by VULCA on 4/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+    int *p;
+    //
+//    int *p_01 = NULL;
+//    int *p_02 = nullptr;
+//
+//    *p_01 = 10;
+//    *p_02 = 20;
+    cout << p << endl;
+//    cout << *p << endl;
+    cout << "hello" << endl;
+
+    return 0;
+}
