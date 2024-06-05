7cabaf2d6e211cfeaf2faa2e9288de3627e8e79f
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-05 08:32:53 +0800

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "H:/Applications/JetBrains/JetBrains_Applications/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "H:/Applications/msys64/mingw64/bin/llvm-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
---
+++ comprehensive_case_of_pointers_and_arrays_learn.cpp
@@ -0,0 +1,66 @@
+//
+// Created by VULCA on 4/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int main() {
+
+//    int *p_array = new int[10]{3, 5, 1, 11, 99, 66, 22, 2, 8, 6};
+//    int *p_temp = new int;
+//    for (int i = 0; i < 10; ++i) {
+//        for (int j = i + 1; j < 10; ++j) {
+//            if (p_array[i] > p_array[j]) {
+//                *p_temp = p_array[i];
+//                p_array[i] = p_array[j];
+//                p_array[j] = *p_temp;
+//            }
+//        }
+//    }
+//    for (int i = 0; i < 10; ++i) {
+//        cout << p_array[i] << endl;
+//    }
+//    delete[] p_array;
+//    delete p_temp;
+
+//    int *p_array = new int[10]{3, 5, 1, 11, 99, 66, 22, 2, 8, 6};
+//    int *p_temp = new int;
+//    for (int i = 0; i < 10; ++i) {
+//        for (int j = 0; j < 10; ++j) {
+//            if (p_array[i] > p_array[j]) {
+//                *p_temp = p_array[i];
+//                p_array[i] = p_array[j];
+//                p_array[j] = *p_temp;
+//            }
+//            for (int k = 0; k < 10; ++k) {
+//                cout << p_array[k] << " \t";
+//            }
+//            cout << endl;
+//        }
+//    }
+//
+//    delete[] p_array;
+//    delete p_temp;
+
+    int *p_array = new int[10]{3, 5, 1, 11, 99, 66, 22, 2, 8, 6};
+    int *p_temp = new int;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = i + 1; j < 10; ++j) {
+            if (p_array[i] > p_array[j]) {
+                *p_temp = p_array[i];
+                p_array[i] = p_array[j];
+                p_array[j] = *p_temp;
+            }
+            for (int k = 0; k < 10; ++k) {
+                cout << p_array[k] << " \t";
+            }
+            cout << endl;
+        }
+    }
+    delete[] p_array;
+    delete p_temp;
+    return 0;
+}
+
+
