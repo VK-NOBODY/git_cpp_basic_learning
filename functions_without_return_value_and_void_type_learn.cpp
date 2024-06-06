91ca3d44076bb94571261d459ddac7c0afb6017c
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-06 18:15:01 +0800

    update functions_without_return_value_and_void_type_learn.cpp

---
+++ functions_without_return_value_and_void_type_learn.cpp
@@ -0,0 +1,16 @@
+//
+// Created by VULCA on 6/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+void say_hello(string name) {
+    cout << name << ",hello,I am Nobody" << endl;
+}
+
+int main() {
+    say_hello("Somebody");
+    say_hello("Anybody");
+    return 0;
+}
