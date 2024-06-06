31161f031fbf22fd060c89c779d3712754494026
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-06 18:18:37 +0800

    update empty_parameter_function_learn.cpp

---
+++ empty_parameter_function_learn.cpp
@@ -0,0 +1,19 @@
+//
+// Created by VULCA on 6/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+void i_like_you() {
+
+    for (int i = 0; i < 5; ++i) {
+        cout << "Xiaomei,I like you!!!" << endl;
+
+    }
+}
+
+int main() {
+    i_like_you();
+    return 0;
+}
