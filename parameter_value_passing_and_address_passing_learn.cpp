2d0ce32943e72a2d55f2eacfab4865e4abed3fca
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-07 10:15:24 +0800

    update parameter_value_passing_and_address_passing_learn.cpp

---
+++ parameter_value_passing_and_address_passing_learn.cpp
@@ -0,0 +1,32 @@
+//
+// Created by VULCA on 7/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+void switch_number(int a, int b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void switch_number_pointer(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main() {
+    int *x = new int(1);
+    int *y = new int(2);
+    switch_number(*x, *y);
+    cout << "x= " << *x << " y= " << *y << endl;
+    switch_number_pointer(x, y);
+    cout << "x= " << *x << " y= " << *y << endl;
+    delete x;
+    x = nullptr;
+    delete y;
+    y = nullptr;
+    return 0;
+}
