ef7079ba8cf728aa165c605f237cadf095967fb5
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-09 16:37:44 +0800

    update function_placeholder_parameters_learn.cpp

---
+++ function_placeholder_parameters_learn.cpp
@@ -0,0 +1,16 @@
+//
+// Created by VULCA on 9/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+// Placeholder parameters can also have default values
+void function_01(int a, int = 10) {
+    cout << "this is function_01" << endl;
+}
+
+int main() {
+    function_01(10, 10);
+    return 0;
+}
