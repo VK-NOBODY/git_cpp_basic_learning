15433f672041059efb4ccf0e4bfe70c453ce293e
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-09 11:36:22 +0800

    update passing_parameters_by_reference_learn.cpp

---
+++ passing_parameters_by_reference_learn.cpp
@@ -0,0 +1,40 @@
+//
+// Created by VULCA on 9/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+void switch_number(int a, int b) {
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void switch_number_pointer(int *a, int *b) {
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void switch_number_reference(int &a, int &b) {
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main() {
+
+    int a = 1, b = 2;
+    cout << "a = " << a << ", b = " << b << endl;
+    switch_number(a, b);
+    cout << "a = " << a << ", b = " << b << endl;
+    switch_number_pointer(&a, &b);
+    cout << "a = " << a << ", b = " << b << endl;
+    switch_number_reference(a, b);
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
