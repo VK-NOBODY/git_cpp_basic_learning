172c1ff0928fd93fe1845e0cb38963e1547fd3ca
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-09 11:48:43 +0800

    update function_return_pointer_and_scope_issues_of_local_variables_learn.cpp

---
+++ function_return_pointer_and_scope_issues_of_local_variables_learn.cpp
@@ -0,0 +1,20 @@
+//
+// Created by VULCA on 9/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int *add(int a, int b) {
+    int *sum = new int;
+    *sum = a + b;
+    return sum;
+}
+
+int main() {
+    int *result_01 = add(10, 20);
+    cout << "result_01: " << *result_01 << endl;
+    delete result_01;
+    result_01 = nullptr;
+    return 0;
+}
