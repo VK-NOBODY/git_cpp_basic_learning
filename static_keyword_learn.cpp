433fad16f648cb14d8b4b461b680874f40ff1074
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-09 11:52:59 +0800

    update static_keyword_learn.cpp

---
+++ static_keyword_learn.cpp
@@ -0,0 +1,18 @@
+//
+// Created by VULCA on 9/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+int *add(int a, int b) {
+    static int p;
+    p = a + b;
+    return &p;
+}
+
+int main() {
+    int *p = add(1, 2);
+    cout << *p << endl;
+    return 0;
+}
