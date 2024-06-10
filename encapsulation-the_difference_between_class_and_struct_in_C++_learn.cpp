603e1d8926f6de8a7248de9609be40bed2c52f0a
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-10 08:35:31 +0800

    update encapsulation-the_difference_between_class_and_struct_in_C++_learn.cpp

---
+++ encapsulation-the_difference_between_class_and_struct_in_C++_learn.cpp
@@ -0,0 +1,23 @@
+//
+// Created by VULCA on 9/6/2024.
+//
+#include "useful_header_file.h"
+
+using namespace std;
+
+class Test_01 {
+public:
+    int a = 10;
+};
+
+struct Test_02 {
+    int b = 10;
+};
+
+int main() {
+    Test_01 test_01;
+    cout << test_01.a << endl;
+    Test_02 test_02;
+    cout << test_02.b << endl;
+    return 0;
+}
