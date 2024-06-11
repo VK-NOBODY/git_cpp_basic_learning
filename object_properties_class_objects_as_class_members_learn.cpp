9b278ee6d15fe11dd63cc62db4072b4a2bde9a80
Vulcankta <Vulcankta@users.noreply.github.com>
2024-06-11 17:24:39 +0800

    update object_properties_class_objects_as_class_members_learn.cpp

---
+++ object_properties_class_objects_as_class_members_learn.cpp
@@ -0,0 +1,43 @@
+//
+// Created by VULCA on 11/6/2024.
+//
+#include "/header_files/useful_header_file.h"
+
+using namespace std;
+
+class Phone {
+public:
+    Phone(string temp_phone_name) {
+        phone_name = temp_phone_name;
+    }
+
+    string phone_name;
+
+    ~Phone() {
+        cout << "Phone " << phone_name << " is destroyed" << endl;
+    }
+};
+
+class Person {
+public:
+    Person(string temp_person_name, string temp_person_phone_name) : person_name(temp_person_name),
+                                                                     person_phone_name(temp_person_phone_name) {
+        cout << person_name << "'s phone name is " << person_phone_name.phone_name << endl;
+    }
+
+    string person_name;
+    Phone person_phone_name;
+
+    ~Person() {
+        cout << "Person " << person_name << " is destroyed" << endl;
+    }
+};
+
+void test_01() {
+    Person person_01("Nobody", "HUAWEI");
+}
+
+int main() {
+    test_01();
+    return 0;
+}
